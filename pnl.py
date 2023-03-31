2017dbb058f75d7c1e43382362f39b8c4b9eea4b
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2023-03-31 09:22:21 +0530

    Added Multichain

--- pnl.py
+++ pnl.py
@@ -5,10 +5,13 @@ api_key = "TL5rt69l60RFyJV97j88BloPRLvnNHt2c9NCDb2f3TMqOYfu6IdKmtdJmBrJUjVb"
 
 address="0xb4C87e51eF9f3D978a6025Eb1fC0869cc65E878F"
 
-def getPrice(address):
+chains=["eth","bsc","polygon","arbitrum","avalanche"]
+
+def getPrice(address,chain):
+  dex={"eth":"uniswap-v2","bsc":"pancakeswap-v1","polygon":"uniswap-v2","arbitrum":"uniswap-v2","avalanche":"uniswap-v2"}
   params = {
-  "chain": "eth",
-  "exchange": "uniswap-v2",
+  "chain": chain,
+  "exchange": dex[chain],
   "address": address
 }
   result = evm_api.token.get_token_price(
@@ -17,7 +20,7 @@ def getPrice(address):
 )
   return result
 
-def getPortfolioValue(balance):
+def getPortfolioValue(balance,chain):
     totalValue=0
     for x in balance:
         
@@ -26,7 +29,7 @@ def getPortfolioValue(balance):
             continue
         else:
             try:
-                price = getPrice(x["token_address"])
+                price = getPrice(x["token_address"],chain)
             except:
                 continue    
             else:    
@@ -35,120 +38,122 @@ def getPortfolioValue(balance):
                 value = (float(x['balance'])/10**x['decimals']) * price['usdPrice']
                 # print(value)
                 totalValue = totalValue + value
-    return totalValue 
+    return totalValue
+
+def getPNL(chain):
+    params = {
+    "chain": chain,
+    "token_addresses": [],
+    "address": address
+    }
+
+    balancePresent = evm_api.token.get_wallet_token_balances(
+    api_key=api_key,
+    params=params,
+    )
+
+    presentValue = getPortfolioValue(balancePresent,chain)
+    print("Present",presentValue)
+    # print(int(time.time()))
+    # print(int(time.time())-86400)
+
+    ########################
+    ########################
+    #       1 Day          # 
+    ########################
+    ########################
+
+    params = {
+    "chain": chain,
+    "date": str(int(time.time())-86400)
+    }
+
+    oneDayBlock = evm_api.block.get_date_to_block(
+    api_key=api_key,
+    params=params,
+    )
+
+    params = {
+    "chain": chain,
+    "to_block": oneDayBlock['block'],
+    "token_addresses": [],
+    "address": address
+    }
+
+    balanceOneDay = evm_api.token.get_wallet_token_balances(
+    api_key=api_key,
+    params=params,
+    )
+
+    oneDayValue = getPortfolioValue(balanceOneDay,chain)
+    print("1 Day Balance",oneDayValue)
+    print("1 Day PNL",presentValue-oneDayValue)
 
 
-params = {
-  "chain": "eth",
-  "token_addresses": [],
-  "address": address
-}
-
-balancePresent = evm_api.token.get_wallet_token_balances(
-  api_key=api_key,
-  params=params,
-)
-
-presentValue = getPortfolioValue(balancePresent)
-print("Present",presentValue)
-# print(int(time.time()))
-# print(int(time.time())-86400)
-
-########################
-########################
-#       1 Day          # 
-########################
-########################
-
-params = {
-  "chain": "eth",
-  "date": str(int(time.time())-86400)
-}
-
-oneDayBlock = evm_api.block.get_date_to_block(
-  api_key=api_key,
-  params=params,
-)
-
-params = {
-  "chain": "eth",
-  "to_block": oneDayBlock['block'],
-  "token_addresses": [],
-  "address": address
-}
-
-balanceOneDay = evm_api.token.get_wallet_token_balances(
-  api_key=api_key,
-  params=params,
-)
-
-oneDayValue = getPortfolioValue(balanceOneDay)
-print("1 Day Balance",oneDayValue)
-print("1 Day PNL",presentValue-oneDayValue)
+    ########################
+    ########################
+    #       7 Day          # 
+    ########################
+    ########################
 
 
-########################
-########################
-#       7 Day          # 
-########################
-########################
+    params = {
+    "chain": chain,
+    "date": str(int(time.time())-(86400*7))
+    }
+
+    sevenDayBlock = evm_api.block.get_date_to_block(
+    api_key=api_key,
+    params=params,
+    )
+
+    params = {
+    "chain": chain,
+    "to_block": sevenDayBlock['block'],
+    "token_addresses": [],
+    "address": address
+    }
+
+    balanceSevenDay = evm_api.token.get_wallet_token_balances(
+    api_key=api_key,
+    params=params,
+    )
+
+    sevenDayValue = getPortfolioValue(balanceSevenDay,chain)
+    print("7 Day Balance",sevenDayValue)
+    print("7 Day PNL",presentValue-sevenDayValue)
+
+    ########################
+    ########################
+    #       30 Day         # 
+    ########################
+    ########################
 
 
-params = {
-  "chain": "eth",
-  "date": str(int(time.time())-(86400*7))
-}
+    params = {
+    "chain": chain,
+    "date": str(int(time.time())-(86400*30))
+    }
 
-sevenDayBlock = evm_api.block.get_date_to_block(
-  api_key=api_key,
-  params=params,
-)
+    thirtyDayBlock = evm_api.block.get_date_to_block(
+    api_key=api_key,
+    params=params,
+    )
 
-params = {
-  "chain": "eth",
-  "to_block": sevenDayBlock['block'],
-  "token_addresses": [],
-  "address": address
-}
+    params = {
+    "chain": chain,
+    "to_block": thirtyDayBlock['block'],
+    "token_addresses": [],
+    "address": address
+    }
 
-balanceSevenDay = evm_api.token.get_wallet_token_balances(
-  api_key=api_key,
-  params=params,
-)
+    balanceThirtyDay = evm_api.token.get_wallet_token_balances(
+    api_key=api_key,
+    params=params,
+    )
 
-sevenDayValue = getPortfolioValue(balanceSevenDay)
-print("7 Day Balance",sevenDayValue)
-print("7 Day PNL",presentValue-sevenDayValue)
+    thirtyDayValue = getPortfolioValue(balanceThirtyDay,chain)
+    print("30 Day Balance",thirtyDayValue)
+    print("30 Day PNL",presentValue-thirtyDayValue)
 
-########################
-########################
-#       30 Day         # 
-########################
-########################
-
-
-params = {
-  "chain": "eth",
-  "date": str(int(time.time())-(86400*30))
-}
-
-thirtyDayBlock = evm_api.block.get_date_to_block(
-  api_key=api_key,
-  params=params,
-)
-
-params = {
-  "chain": "eth",
-  "to_block": thirtyDayBlock['block'],
-  "token_addresses": [],
-  "address": address
-}
-
-balanceThirtyDay = evm_api.token.get_wallet_token_balances(
-  api_key=api_key,
-  params=params,
-)
-
-thirtyDayValue = getPortfolioValue(balanceThirtyDay)
-print("30 Day Balance",thirtyDayValue)
-print("30 Day PNL",presentValue-thirtyDayValue)
+getPNL("eth")
